3173e7b4aeb0796f01255857433934e61f797861
nanaki14 <masatochiba0122@gmail.com>
2019-05-27 12:02:51 +0900

    feat: 🎸 initial

---
+++ src/assets/javascript/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import {render} from 'react-dom'
+
+import Title from './components/Title'
+
+render(<Title align="center">こんにちわ世界</Title>, document.getElementById('app'))
